47debafc32083971c4a3b529bb73e0dd4bf87497
AhmedReda958 <asateralcomedi123@gmail.com>
2023-01-28 21:23:47 +0200

    tesimonials section

--- src/components/TestimonialsSlider/index.jsx
+++ src/components/TestimonialsSlider/index.jsx
@@ -1,14 +1,124 @@
 import React from "react";
+import "./style.scss";
+import { Carousel } from "react-responsive-carousel";
+
+import avatar from "assets/images/testimonials-avatar.png";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const TestmonialsSlider = () => {
+  const renderArrow =
+    (direction) => (onClickHandler, shouldBeEnabled, label) => {
+      if (!shouldBeEnabled) {
+        return;
+      }
+
+      return direction == "next" ? (
+        <button className="swipe-button right-arrow" onClick={onClickHandler}>
+          <FaArrowRight />
+        </button>
+      ) : (
+        <button
+          className="swipe-button left-arrow"
+          type="button"
+          onClick={onClickHandler}
+        >
+          <FaArrowLeft />
+        </button>
+      );
+    };
   return (
     <div className="slider">
-      <blockquote>
-        <h5>
-          "The best agency we’ve worked with so far. They understand our product
-          and are able to add new features with a great focus."
-        </h5>
-      </blockquote>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        // swipeable
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        // renderArrowPrev={renderArrow("prev")}
+        renderArrowNext={renderArrow("next")}
+      >
+        <blockquote className="quote ">
+          <h5>
+            The best agency we’ve worked with so far. They understand our
+            product and are able to add new features with a great focus.
+          </h5>
+          <div className="profile">
+            <div className="avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="info">
+              <label className="name">Jenny Wilson</label>
+              <br />
+              <span className="title">Vice President</span>
+            </div>
+          </div>
+        </blockquote>
+        <blockquote className="quote ">
+          <h5>
+            The best agency we’ve worked with so far. They understand our
+            product and are able to add new features with a great focus.
+          </h5>
+          <div className="profile">
+            <div className="avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="info">
+              <label className="name">Jenny Wilson</label>
+              <br />
+              <span className="title">Vice President</span>
+            </div>
+          </div>
+        </blockquote>
+        <blockquote className="quote ">
+          <h5>
+            The best agency we’ve worked with so far. They understand our
+            product and are able to add new features with a great focus.
+          </h5>
+          <div className="profile">
+            <div className="avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="info">
+              <label className="name">Jenny Wilson</label>
+              <br />
+              <span className="title">Vice President</span>
+            </div>
+          </div>
+        </blockquote>
+        <blockquote className="quote ">
+          <h5>
+            The best agency we’ve worked with so far. They understand our
+            product and are able to add new features with a great focus.
+          </h5>
+          <div className="profile">
+            <div className="avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="info">
+              <label className="name">Jenny Wilson</label>
+              <br />
+              <span className="title">Vice President</span>
+            </div>
+          </div>
+        </blockquote>
+        <blockquote className="quote ">
+          <h5>
+            The best agency we’ve worked with so far. They understand our
+            product and are able to add new features with a great focus.
+          </h5>
+          <div className="profile">
+            <div className="avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="info">
+              <label className="name">Jenny Wilson</label>
+              <br />
+              <span className="title">Vice President</span>
+            </div>
+          </div>
+        </blockquote>
+      </Carousel>
     </div>
   );
 };
--- src/pages/Home/sections/testimonials.jsx
+++ src/pages/Home/sections/testimonials.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
 import TestimonialsSlider from "components/TestimonialsSlider";
+
 const Testimonials = () => {
   return (
     <div className="testimonials">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-4">
+          <div className="col-12 col-md-4 pe-md-5 mb-5 mb-md-0 text-center text-md-start">
             <h3>What our clients say about us</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</p>
           </div>
